30bd1799a626b43f88b0ea7248196f1ae2a69681
bmsteven <bmsteve96@gmail.com>
2019-09-26 15:20:36 +0300

    Updates

---
+++ precache-manifest.898a688b1624840ad3a8b7b48db2d82e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f1465da39038fc0cc586c95ae660faf",
+    "url": "/codemunit/index.html"
+  },
+  {
+    "revision": "05ffd68df5f2f558cf2c",
+    "url": "/codemunit/static/css/main.3aaa04cf.chunk.css"
+  },
+  {
+    "revision": "bd1ec80eccabf43fb065",
+    "url": "/codemunit/static/js/2.6b323979.chunk.js"
+  },
+  {
+    "revision": "05ffd68df5f2f558cf2c",
+    "url": "/codemunit/static/js/main.b31ee980.chunk.js"
+  },
+  {
+    "revision": "ef85449152cae0e98325",
+    "url": "/codemunit/static/js/runtime~main.8637205a.js"
+  },
+  {
+    "revision": "e48c08dd4153f01993c3263d93fbdc6e",
+    "url": "/codemunit/static/media/bg-about.e48c08dd.jpg"
+  },
+  {
+    "revision": "41c8c8f404b1a3d72f43a1e010eb5960",
+    "url": "/codemunit/static/media/bg-image.41c8c8f4.jpg"
+  },
+  {
+    "revision": "768a903ccd1dcfd5302398b74c11f668",
+    "url": "/codemunit/static/media/intro.768a903c.jpg"
+  },
+  {
+    "revision": "fee09062f513bd0e84d3e6f8682ca733",
+    "url": "/codemunit/static/media/tech.fee09062.jpeg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/codemunit/precache-manifest.898a688b1624840ad3a8b7b48db2d82e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/codemunit/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
